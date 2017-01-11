f39f9983f6dc4dc196c1f380872312ad6db9dc98
Chiron Martini <WWCLC@asdconnect.org>
2017-01-11 14:28:27 -0800

    chiron change

--- individual/Chiron.py
+++ individual/Chiron.py
@@ -11,25 +11,26 @@ from bwx_adventure.advent import NORTH, SOUTH, EAST, WEST, UP, DOWN, RIGHT, LEFT
 game = Game("Lucky Day")
 
 sidewalk = game.new_location(
-  "Sidewalk
+  "Sidewalk",
   "There is a large glass door to the east. The sign says 'Come In!'")
 
 vestibule = game.new_location(
   "Vestibule",
 """A small area at the bottom of a flight of stairs.
-There is a glass door to the west.""")
+There is a glass door to the east.""")
 
 game.new_connection("Glass Door", sidewalk, vestibule, [IN, EAST], [OUT, WEST])
 
-game.new_location(
+tortureroom = game.new_location(
     "Midieval Torture Room",
 """You see multiple horrifying torture devices in a small stone brick room.
-There is an arched doorway leading into a dark passageway to the west.""")
+There is an arched doorway leading into a dark passageway to the West.""")
 
+darkpassageway = game.new_location(
+    "Dark Passageway",
+"""You can go forward to the North or back into the torture room to the South.""")
 
-
-
-
+game.new_connection("Arched Doorway", tortureroom, darkpassageway [IN, WEST], [OUT, SOUTH])
 
 
 
