377803e375d9c4eb5d3b62f47e418878e15e6e63
odinmartini <odinmartini@gmail.com>
2017-01-12 14:26:46 -0800

    Ody and Dylan game
    Ody and Dylan game

--- group/Ody and Dylan game.py
+++ group/Ody and Dylan game.py
@@ -12,26 +12,34 @@ game = Game("Shadows")
 
 dungeon = game.new_location(
   "Dark Dungeon Cell",
-  "There is a guard just outside your cell with a key chain and a dwarven dagger.")
+  "There is a guard just outside your cell with a key chain and a dwarven dagger, the door to your cell is to the north.")
 
 corridor = game.new_location(
   "Corridor",
-"""You are in long corridor, there is a large open wooden door to the west.""")
+  "You are in long corridor, there is a large open wooden door to the west.")
+
+courtyard = game.new_location(
+  "Courtyard",
+  "You are in a large empty courtyard with dead people littering the floor, there seems to be some kind of lethal disease. There is a large grating to the west and a pathway leading east.")
+
 
 cell_door = game.new_connection("Cell Door", dungeon, corridor, [IN, NORTH], [OUT, SOUTH])
 
+wooden_door = game.new_connection("Wooden Door", corridor, courtyard, [IN, WEST], [OUT, EAST])
+
 player = game.new_player(dungeon)
 
-guard = Animal("guard")
+guard = Actor("guard")
 guard.set_location(dungeon)
 guard.set_allowed_locations([dungeon])
+
 
 torch = Object("torch", "a recently lit torch")
 torch = corridor.new_object("torch", "a recently lit torch")
 
-key_chain = Object("key chain", "a rusty ring of old keys")
-corridor.make_requirement (key_chain)
 dagger = Object("dwarven dagger", "a polished dwarven dagger")
+key_chain = Object("key chain", "a rusty ring of old keys")
+cell_door.make_requirement (key_chain)
 guard.add_to_inventory(key_chain)
 guard.add_to_inventory(dagger)
 
@@ -41,19 +49,31 @@ def unlock_door(game, thing):
   thing.unset_flag('locked')
 cell_door.add_phrase('unlock door', unlock_door, [key_chain])
 
+cell_door.set_flag('locked')
+def move_grate(game, thing):
+  game.output("")
+  thing.unset_flag('locked')
+cell_door.add_phrase('move grate', move_grate, [])
+
 sharp_bone = dungeon.new_object("sharp bone", "a sharp bone lies in the corner to the left of you")
 
 def kill_guard(game, thing):
-  if not "sharp_bone" in game.player.inventory:
+  if not "sharp bone" in game.player.inventory:
     game.output("You try to attack the guard with your bare hands, but he wacks you with the bunt of his blade.")
     player.terminate()
   else:
-    game.output("You stab the guard from behind at the base of his neck, and he drops to the ground dead.")
+    game.output("You stab the guard from behind at the base of his neck, and he drops to the ground dead. The guard had a key chain and a dwarven dagger, they could be useful.")
 
     guard.terminate()
-    guard.act_drop1(player,key_chain)
-    guard.act_drop1(player,dagger)
+    guard.act_drop1(player,"key chain","The guard dropped a key chain.")
+    guard.act_drop1(player,"dwarven dagger","The guard dropped a dwarven dagger")
 guard.add_phrase("kill guard", kill_guard)
- 
+
+#unset locations
+
+
+
+
+
 game.run()
 
