e6c92d16e5928052b3595642ef44c27124f074ba
sleepinghungry <sleepinghungry@gmail.com>
2017-01-14 00:40:35 -0800

    Minor enhancments.

--- homework/items.py
+++ homework/items.py
@@ -1,29 +1,19 @@
 #!/user/bin/python
 # vim: et sw=2 ts=2 sts=2
-#
-# This is a tutorial for writing Interactive Fiction with the BWX Adventure Game Engine.
-# Ignore the magic before this point (feel free to ask if you are curious).
-#
-# First we need to import everything we need from the Game engine (a module called 'advent'):
 
-# Allows access to the bwx_adventure module
 import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
-from bwx_adventure.advent import *
-# for cloud9
-from bwx_adventure.advent import Game, Location, Connection, Object, Animal, Robot, Pet, Player, Say, SayOnNoun, SayOnSelf
+from bwx_adventure.advent import Game, Location, Connection, Object, Animal, Robot, Pet, Player, Say, SayOnNoun, SayOnSelf, Food, Drink, Container, Die
 from bwx_adventure.advent import NORTH, SOUTH, EAST, WEST, UP, DOWN, RIGHT, LEFT, IN, OUT, FORWARD, BACK, NORTH_WEST, NORTH_EAST, SOUTH_WEST, SOUTH_EAST, NOT_DIRECTION
 
-game = Game("Brightworks Adventure")
-
+game = Game("Willow Wind Adventure")
 
 #############################################################
 # HOMEWORK STARTS HERE
 #
 # 1. Open items_list.txt and keep it open next to this file.
-# 2. Use the locations in items_list.txt to
+# 2. Use the items in items_list.txt to
 #    Write 2 new object commands (1 ball, 1 key)
 #          1 make required commands (key required to open office door)
 #          1 add phrase commands (bounce ball)
@@ -40,7 +30,7 @@ office = game.new_location("Office", "This place is a mess.")
 upstairs = game.new_location("Upstairs Hall", "You are upstairs")
 
 # Connections
-front_door = game.new_connection("Front Door",front, vestibule, IN, OUT)
+front_door = game.new_connection("Front Door",front, vestibule, [IN,EAST], [OUT,WEST])
 office_door = game.new_connection("Office Door", vestibule, office, IN, OUT)
 stairs = game.new_connection("Upstairs", vestibule, upstairs, UP, DOWN)
 
--- homework/locations.py
+++ homework/locations.py
@@ -1,46 +1,34 @@
 #!/user/bin/python
 # vim: et sw=2 ts=2 sts=2
-#
-# This is a tutorial for writing Interactive Fiction with the BWX Adventure Game Engine.
-# Ignore the magic before this point (feel free to ask if you are curious).
-#
-# First we need to import everything we need from the Game engine (a module called 'advent'):
 
-# Allows access to the bwx_adventure module
 import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
-from bwx_adventure.advent import *
-# for cloud9
-from bwx_adventure.advent import Game, Location, Connection, Object, Animal, Robot, Pet, Player
+from bwx_adventure.advent import Game, Location, Connection, Object, Animal, Robot, Pet, Player, Say, SayOnNoun, SayOnSelf, Food, Drink, Container, Die
 from bwx_adventure.advent import NORTH, SOUTH, EAST, WEST, UP, DOWN, RIGHT, LEFT, IN, OUT, FORWARD, BACK, NORTH_WEST, NORTH_EAST, SOUTH_WEST, SOUTH_EAST, NOT_DIRECTION
 
-game = Game("Brightworks Adventure")
-
+game = Game("WIllow Wind Adventure")
 
 #############################################################
 # HOMEWORK STARTS HERE
 #
 # 1. Open locations_list.txt and keep it open next to this file.
-# 2, Use the locations in locations_list.txt to
+# 2. Use the locations in locations_list.txt to
 #    Write 4 new location commands
 #          3 new connection commands, and
 #          1 command to create a player
 #
-# 
-##### REMEMBER: commands have () after them
-#####           info needed by the command is put inside the ()
-#####           If more than one piece of info is needed, separate the pieces of info with a comma.
-#
+# Use can highlight text and use CTRL-C to copy, and CTRL-V to paste.
 #
 # Handy Dandy Reference:
 #  https://github.com/sleepinghungry/wwif/wiki/Locations-and-Connections-Homework
 #
+#############################################################
 
 #####################
 # START CODING HERE #
 #####################
+
 
 #################
 # END CODE HERE #
