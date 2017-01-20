e5ddc7251f5c72715404431434fefcc5347daf7f
Shaners55 <shanesari@rocketmail.com>
2017-01-20 13:16:05 -0800

    shanes file

---
+++ individual/shane1.py
@@ -0,0 +1,30 @@
+
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+from bwx_adventure.advent import *
+
+from bwx_adventure.advent import Game, Location, Connection, Object, Animal, Robot, Pet, Player
+from bwx_adventure.advent import NORTH, SOUTH, EAST, WEST, UP, DOWN, RIGHT, LEFT, IN, OUT, FORWARD, BACK, NORTH_WEST, NORTH_EAST, SOUTH_WEST, SOUTH_EAST, NOT_DIRECTION
+
+game = Game("World Warp-Z")
+
+local_1 = game.new_location(
+  "Office Door",
+  "There is a door that has red stains on the handle, to the East.")
+
+Death = game.new_location(
+  "Dark Alley",
+"""If you go west you will get eaten by the Hord.""")
+
+
+
+game.new_connection("Glass Door", Local_1, Death, [IN, EAST], [OUT, WEST])
+
+
+
+player = game.new_player(Local_1)
+
+
+game.run()
