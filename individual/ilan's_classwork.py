fb97f3c770c782f4e5d74f06b60a11baaef34b74
ilanrsmith <ilanrsmith@gmail.com>
2017-04-04 14:59:56 -0700

    python

--- individual/ilan's_classwork.py
+++ individual/ilan's_classwork.py
@@ -1,14 +1,31 @@
 import random
 import turtle
 turtle.colormode(255)
-
 turtle.speed(0)
 
+
+def draw_shape():
+    turtle.goto(random.randint(-200,200), random.randint(-200,200))
+    random_number = random.randint(0,500)
+    turtle.pencolor(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    turtle.pensize(random.randint(1,30))
+    def square():
+        for i in range(4):
+            turtle.forward(random_number)
+            turtle.right(90)
+
+    def triangle():
+        for i in range(3):
+            turtle.forward(random_number)
+            turtle.left(120)
+    def hexegon():
+        for i in range(6):
+            turtle.forward(random_number)
+            turtle.right(120) 
+    def octogon():
+        for i in range(8):
+            turtle.forward(random_number)
+            turtle.right(315)
+
 while True:
-    turtle.goto(random.randint(0,200), random.randint(0,200))
-    turtle.pencolor(random.randint(0,155), random.randint(0,155), random.randint(0,155))
-    turtle.pensize(random.randint(1,50))
-    for i in range(random.randint(1,360)):
-        turtle.forward(random.randint(1,300))
-        turtle.right(random.randint(1,500))
-        
+    draw_shape
