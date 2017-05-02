6bd70ea1c0e7d711eefd0773c307f475361583ee
Shaners55 <shanesari@rocketmail.com>
2017-05-02 14:31:46 -0700

    shanes work

--- individual/llllllllllllllloooooooooooooooooooooooooolllllllllllllllllll.py
+++ individual/llllllllllllllloooooooooooooooooooooooooolllllllllllllllllll.py
@@ -1,6 +1,103 @@
-import random
+#dumb = (
+ #   ikillu = (input("Welcome to the diving score calculator. Please enter a difficulty:"))
+ #     
+ #   two = input("What is the first judjes score?")
+    
+ #   three = input("What is the second judjes score?")
+    
+ #   four = input("What is the third judjes score?")
+    
+ #   five = input("What is the fourth judjes score?")
+    
+ #   six = input("What is the fifth judjes score?")
+    
+ #   one_through_six = [two, three, four, five, six]
+    
+ #   one_through_six.remove(max(one_through_six))
+    
+ #   one_through_six.remove(min(one_through_six))
+    
+ #   one_through_six123 = one_through_six[0] + one_through_six[1] + one_through_six[2])
 
-#scores = ("10.0", "9.5", "9.0", "8.5", "8.0", "7.5", "7.0", "6.5", "6.0", "5.5", "5.0")
 
-input("Please enter the degree of difficulty?")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dif = float (input("Welcome to the diving score calculator. Please enter a difficulty:"))
+score1 = float (input("Enter a score:"))
+score2 = float (input("Enter a score:"))
+score3 = float (input("Enter a score:"))
+score4 = float (input("Enter a score:"))
+score5 = float (input("Enter a score:"))
+scores = [score1, score2, score3, score4, score5]
+scores.remove(max(scores))
+scores.remove(min(scores))
+total = scores[0] + scores[1] + scores[2]
+print (total*dif)
