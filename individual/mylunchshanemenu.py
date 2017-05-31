82bb080a4feec0ead1c2a1d07e415ab19142a82d
Shaners55 <shanesari@rocketmail.com>
2017-05-31 09:30:02 -0700

    shanemich

---
+++ individual/mylunchshanemenu.py
@@ -0,0 +1,10 @@
+softdrinks = ("Soda",
+              "$2.05")
+peachsmoothie = ("Peach Smoothie",
+                 "$3.05")
+Cofee = ("Cofee",
+         "$3.05")
+milk = ("Goat Or Cow Milk",
+        "$3.05")
+chocomilk = ("Chockolate Coconut Milk",
+             "$3.05")
