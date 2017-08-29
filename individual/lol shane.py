e247a8ee73653627feb6a27538cf24de9364c24c
sleepinghungry <sleepinghungry@gmail.com>
2017-08-28 22:08:40 -0700

    Fall cleaning before semester starts.

--- individual/lol shane.py
+++
@@ -1,2 +0,0 @@
-import shane agee shape
-
--- individual/shane has number game.py
+++
@@ -1,9 +0,0 @@
-user1_name = input("Player one what be your name.")
-
-print ("On the table is a 2014 Camero.")
-
-print ("A 7 day trip to Alaska.")
-
-print ("And peanut butter and jelly sandwich")
-
-playg1 = input("Player 1 guess the price for these items and trip.")
