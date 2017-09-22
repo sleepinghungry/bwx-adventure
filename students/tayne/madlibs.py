d9d8bb0fea4c97ed690bef896260a3623e02a2e9
sleepinghungry <sleepinghungry@gmail.com>
2017-09-22 08:25:14 -0700

    student update

---
+++ students/tayne/madlibs.py
@@ -0,0 +1,23 @@
+print
+(" wow, I love the smell of",
+       input ("holaday"),
+       ". I get to play",
+       input ("video game"),
+       "on my",
+       input ("electronic or concle"),
+       ". my mom hates when i play it but my dad is very",
+       input ("feeling"),
+       "so he dosen't care at all. yesterday my sister took an",
+       input ("sharp object"),
+       "to my",
+       input ("electronic or concle"),
+       ". i punched her untill she bacicly",
+       input ("way to get around"),
+       "to",
+       input ("a place"),
+       ".i was satisfied now on my",
+       input ("electronic or concle"),
+       ".holowene was",
+       input ("great awesome bad horid something like that"),
+       ".")
+
